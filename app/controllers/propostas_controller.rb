67b2cb91037827ca9c03504b925c4696ff28692f
Renato Fialho <fialhorenato@users.noreply.github.com>
2017-10-13 16:35:57 -0300

    Organizing the controllers and routes

---
+++ app/controllers/propostas_controller.rb
@@ -0,0 +1,33 @@
+class PropostasController < ApplicationController
+
+    def index
+        @proposta = Proposta.new
+        render 'new'
+    end
+    
+    def new
+        @proposta = Proposta.new
+    end
+
+    def create
+        @proposta = Proposta.new(proposta_params)
+        
+        if @proposta.valid?
+            @proposta.calcular()
+            @proposta.save
+            redirect_to proposta_url(@proposta)
+        else
+            render 'new'
+        end
+        
+    end
+
+    def show
+        @proposta = Proposta.find(params[:id])
+    end
+
+    private
+    def proposta_params
+      params.require(:proposta).permit(:nome, :nomedaempresa, :email, :faturamento, :valordotitulo, :prazo)
+    end
+end
